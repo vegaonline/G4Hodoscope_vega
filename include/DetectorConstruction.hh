6a0fafe102e1fac43f9987add3e274e95dc72a23
Abhijit Bhattacharyya <abhihere@gmail.com>
2016-07-29 17:09:53 +0530

    Minor modification...

--- include/DetectorConstruction.hh
+++ include/DetectorConstruction.hh
@@ -12,37 +12,37 @@ class G4VSensitiveDetector;
 class G4VisAttributes;
 
 class DetectorConstruction : public G4VUserDetectorConstruction {
-	public :
-		DetectorConstruction();
-		virtual ~DetectorConstruction();
+  public:
+    DetectorConstruction();
+    virtual ~DetectorConstruction();
 
-	public :
-		virtual G4VPhysicalVolume* Construct();
+  public:
+    virtual G4VPhysicalVolume* Construct();
 
-	private:
-		G4Material* air;
-		G4Material* argongas;
-		G4Material* scintillator;
-		G4Material* lead;
-		G4Material* CsI;
+  private:
+    G4Material* air;
+    G4Material* argongas;
+    G4Material* scintillator;
+    G4Material* lead;
+    G4Material* CsI;
 
-		G4VisAttributes* worldVisAtt;
-		G4VisAttributes* hodoscopeVisAtt;
-		G4VisAttributes* chamberVisAtt;
+    G4VisAttributes* worldVisAtt;
+    G4VisAttributes* hodoscopeVisAtt;
+    G4VisAttributes* chamberVisAtt;
 
+    G4double deltaStacks; // difference between upper and lower stack where object stays
+    G4double deltaDet;    // gap between each consecutive detector in either upper or lower
+    G4double detSide1;    // must be length side
+    G4double detSide2;    // must be breadth side
+    G4double detSide3;    // must be length side
+    G4double detSide4;    // must be breadth side
+    G4double detHeight;
 
-		G4double deltaStacks; // difference between upper and lower stack where object stays
-		G4double deltaDet;    // gap between each consecutive detector in either upper or lower
-		G4double detSide1;	  // must be length side
-		G4double detSide2;	  // must be breadth side
-		G4double detSide3;    // must be length side
-		G4double detSide4;    // must be breadth side
-		G4double detHeight;
+    G4double objLen;
+    G4double objWid;
+    G4double objHt;
 
-		G4double objLen;
-		G4double objWid;
-		G4double objHt;
-
-
+    G4Int numDet;        // total number of detectors 3 top and 3 bottom
+    G4Int numChanPerDet; // total number of channels per detectors
 }
 #endif
--- src/DetectorConstruction.cc
+++ src/DetectorConstruction.cc
@@ -19,74 +19,42 @@
 #include "G4VisAttributes.hh"
 #include "G4Colour.hh"
 
-
-
-
-
-
-
-
-
-
-
-
-
 DetectorConstruction::DetectorConstruction()
-	: air(0), argongas(0), scintillator(0), CsI(0), lead(0), 
-	detSide1(160.0*cm), detSide2(90.0*cm), detSide3(160.0*cm), detSide4(60.0*cm),
-	detHeight(5.0*cm), deltaDet(3.0*cm), deltaStacks(45.0*cm), 
-	objLen(10.0*cm), objWid(10.0*cm), objHt(10.0cm),
-	  worldVisAtt(0), hodoscopeVisAtt(0), chamberVisAtt(0)
+    : air(0), argongas(0), scintillator(0), CsI(0), lead(0), detSide1(160.0 * cm), detSide2(90.0 * cm),
+      detSide3(160.0 * cm), detSide4(60.0 * cm), detHeight(5.0 * cm), deltaDet(3.0 * cm), deltaStacks(45.0 * cm),
+      objLen(10.0 * cm), objWid(10.0 * cm), objHt(10.0cm), numDet(6), numChanPerDet(32), worldVisAtt(0),
+      hodoscopeVisAtt(0), chamberVisAtt(0)
 {
-
-
 }
 
-DetectorConstruction::~DetectorConstruction() {
-	DestroyMaterials();
+DetectorConstruction::~DetectorConstruction()
+{
+    DestroyMaterials();
 
-	delete worldVisAtt;
-	delete hodoscopeVisAtt;
-	delete chamberVisAtt;
+    delete worldVisAtt;
+    delete hodoscopeVisAtt;
+    delete chamberVisAtt;
 }
 
-G4VPhysicalVolume* DetectorConstruction::Construct() {
-	G4VSensitiveDetector* hodoscope;
-	G4VSensitiveDetector* chamber1;
+G4VPhysicalVolume* DetectorConstruction::Construct()
+{
+    G4VSensitiveDetector* hodoscope;
+    G4VSensitiveDetector* chamber1;
 
-	ConstructMaterials();
+    ConstructMaterials();
 
-	// geometry ******
-	G4double hodoLen = 1.5 * max(detSide1, detSide3);
-	G4double hodoWid = 1.5 * max(detSide3, detSide4);
-	G4double hodoHt  = 6 * deltaDet + deltaStacks + 6 * detHeight; 
+    // geometry ******
+    G4double hodoLen = 1.5 * max(detSide1, detSide3);
+    G4double hodoWid = 1.5 * max(detSide3, detSide4);
+    G4double hodoHt = 6 * deltaDet + deltaStacks + 6 * detHeight;
 
-	
-	// Experimental Hall (world volume)
-	G4VSolid* worldSolid = new G4Box("worldBox", 2.0*m, 2.0*m, 5.0*m);
-	G4LogicalVolume* worldLogical = new G4LogicalVolume(worldSolid, air, "worldLogical", 0,0,0);
-	G4VPhysicalVolume* worldPhysical = new G4PVPlacement(0, G4ThreeVector(), worldLogical, "worldPhysical", 0, 0, 0);
+    // Experimental Hall (world volume)
+    G4VSolid* worldSolid = new G4Box("worldBox", 2.0 * m, 2.0 * m, 5.0 * m);
+    G4LogicalVolume* worldLogical = new G4LogicalVolume(worldSolid, air, "worldLogical", 0, 0, 0);
+    G4VPhysicalVolume* worldPhysical = new G4PVPlacement(0, G4ThreeVector(), worldLogical, "worldPhysical", 0, 0, 0);
 
-	// Place the hodoscope in the world volume
-	G4VSolid* hodoscopeSolid = new G4Box("hodoscopeBox", hodoLen, hodoWid, hodoHt);
-	G4LogicalVolume* hodoscopeLogical = new G4LogicalVolume(hodoscopeSolid, 
-
-
-
-
-
-
+    // Place the hodoscope in the world volume
+    G4VSolid* hodoscopeSolid = new G4Box("hodoscopeBox", hodoLen, hodoWid, hodoHt);
+    G4LogicalVolume* hodoscopeLogical = new G4LogicalVolume(hodoscopeSolid, scintillator, "hodoscopeogical", 0, 0, 0);
+    new G4PVPlacement(0, G4ThreeVector(0., 0., 0.), hodoscopeLogical, "hodoscopePhysical", worldLogical, 0, 0);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
